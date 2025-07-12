98f3be2e48d10922fd3834ea40511ccd3abcf5fd
Leandro H Manica <leandrohmanica@gmail.com>
2025-07-12 20:27:58 -0300

    Images path defaults to app storage dir (#5)

--- config/environment.rb
+++ config/environment.rb
@@ -7,11 +7,15 @@ Rails.application.initialize!
 # transform key to camel case for json responses
 Jbuilder.key_format camelize: :lower
 
-FALAE_IMAGES_PATH = ENV['FALAE_IMAGES_PATH']
-
-if FALAE_IMAGES_PATH.blank? || ! Dir.exist?(FALAE_IMAGES_PATH)
-  raise 'FALAE_IMAGES_PATH is not set properly'
-end
+FALAE_IMAGES_PATH = if ENV['FALAE_IMAGES_PATH']
+    if !Dir.exist?(ENV['FALAE_IMAGES_PATH'])
+      raise "Directoy path set by FALAE_IMAGES_PATH env var (#{ENV['FALAE_IMAGES_PATH']}) does not exist."
+    else
+      ENV['FALAE_IMAGES_PATH']
+    end
+  else
+    Rails.root.join('storage')
+  end
 
 Dir.mkdir "#{FALAE_IMAGES_PATH}/public" unless Dir.exist? "#{FALAE_IMAGES_PATH}/public"
 Dir.mkdir "#{FALAE_IMAGES_PATH}/private" unless Dir.exist? "#{FALAE_IMAGES_PATH}/private"
